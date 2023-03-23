792c5b503f865042f26f0e14282afd34393c2c85
Sergio Guzman <sealguzmanal@ittepic>
2023-03-23 08:53:06 -0700

    Version 2.0

---
+++ src/app/services/task.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+  private completedTask: string[] = [];
+  private tasks: string[] = [];
+  constructor() { 
+    this.tasks.push("Tarea 1");
+  }
+  public getTasks(): string[]{
+    return this.tasks;
+  }
+  public getCompletedTask(): string[]{
+    return this.completedTask;
+  }
+  public completeTask(pos:number){
+    this.completedTask.push(this.tasks[pos]);
+    this.tasks.splice(pos,1);
+    return this.tasks;
+  }
+  public newTask (task:string){
+    this.tasks.push(task);
+    return this.tasks;
+  }
+}
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,39 +1,14 @@
 import { Component } from '@angular/core';
-
+import { AlertController, IonInput, IonList, ToastController, } from '@ionic/angular';
+import { TaskService } from '../services/task.service';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-  public products: { name: string, price: number, type: string }[] = [
-    { name:"Coca Cola 600ml",price:20,type:"abarrotes" },
-    { name: 'Pepsi 600ml', price: 20, type: 'abarrotes' },
-    { name: 'Queso cotija', price: 30, type: 'abarrotes' },
-    { name: 'Croquetas Pedigree', price: 40, type: 'mascotas' },
-    { name: 'Whiskas', price: 20, type: 'mascotas' },
-    { name: 'Alpiste', price: 13, type: 'mascotas' },
-    { name: 'Fabuloso', price: 25, type: 'limpieza' },
-    { name: 'Cloro', price: 25, type: 'limpieza' },
-    { name: 'Pinol', price: 20, type: 'limpieza' },
-  ];
-  constructor() {}
-  etColor(type: String) : string{
-    let color = "";
-
-    switch(type){
-      case "Abarrotes":
-        color = "primary";
-        break;
-      case "Limpieza":
-        color = "success";
-        break;
-      case "Mascotas":
-        color = "warning";
-        break;  
-      default:
-        break;
-    }
-    return color;
+  listTasks: string[] = [];
+  constructor(private toastr: ToastController, private tasks:TaskService) {
+    this.listTasks = this.tasks.getCompletedTask();
   }
 }
