4f9966b6d3e6e97c526b8dfbaed81fa472416850
Sergio Guzman <sealguzmanal@ittepic>
2023-03-23 00:26:20 -0700

    Version 1.0

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
+  public products: { name: string, price: number, type: string }[] = [
+    { name:"Coca Cola 600ml",price:20,type:"abarrotes" },
+    { name: 'Pepsi 600ml', price: 20, type: 'abarrotes' },
+    { name: 'Queso cotija', price: 30, type: 'abarrotes' },
+    { name: 'Croquetas Pedigree', price: 40, type: 'mascotas' },
+    { name: 'Whiskas', price: 20, type: 'mascotas' },
+    { name: 'Alpiste', price: 13, type: 'mascotas' },
+    { name: 'Fabuloso', price: 25, type: 'limpieza' },
+    { name: 'Cloro', price: 25, type: 'limpieza' },
+    { name: 'Pinol', price: 20, type: 'limpieza' },
+  ];
   constructor() {}
+  getColor(type: String) : string{
+    let color = "";
 
+    switch(type){
+      case "abarrotes":
+        color = "primary";
+        break;
+      case "limpieza":
+        color = "success";
+        break;
+      case "mascotas":
+        color = "warning";
+        break;  
+      default:
+        break;
+    }
+    return color;
+  }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,7 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-
+  public products: { name: string, price: number, type: string }[] = [
+    { name:"Coca Cola 600ml",price:20,type:"abarrotes" },
+    { name: 'Pepsi 600ml', price: 20, type: 'abarrotes' },
+    { name: 'Queso cotija', price: 30, type: 'abarrotes' },
+    { name: 'Croquetas Pedigree', price: 40, type: 'mascotas' },
+    { name: 'Whiskas', price: 20, type: 'mascotas' },
+    { name: 'Alpiste', price: 13, type: 'mascotas' },
+    { name: 'Fabuloso', price: 25, type: 'limpieza' },
+    { name: 'Cloro', price: 25, type: 'limpieza' },
+    { name: 'Pinol', price: 20, type: 'limpieza' },
+  ];
   constructor() {}
+  etColor(type: String) : string{
+    let color = "";
 
+    switch(type){
+      case "Abarrotes":
+        color = "primary";
+        break;
+      case "Limpieza":
+        color = "success";
+        break;
+      case "Mascotas":
+        color = "warning";
+        break;  
+      default:
+        break;
+    }
+    return color;
+  }
 }
